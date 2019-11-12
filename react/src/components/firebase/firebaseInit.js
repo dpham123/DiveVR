91596712c332e1664c6c491b7d80e0f61b32a5d9
Dominic Pham <36905285+dpham123@users.noreply.github.com>
2019-11-12 11:26:53 -0800

    Migrate stuff

--- react/src/components/firebase/firebaseInit.js
+++ react/src/components/firebase/firebaseInit.js
@@ -1,5 +1,14 @@
 import firebase from 'firebase'
-import { firebaseConfig } from "../config/config.js"
+
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
+}
 
 export var fire = firebase.initializeApp(firebaseConfig)
 export var storage = firebase.storage()
